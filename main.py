25720b000fa5850992e5e342a2c1f7083f44ea42
GamalielAbad12 <124740682+GamalielAbad12@users.noreply.github.com>
2023-08-11 00:25:03 -0600

    "main"

---
+++ main.py
@@ -0,0 +1,15 @@
+import funcion_uno 
+import funcion_dos
+import funcion_tres
+import funcion_cuatro
+     
+    
+def main():
+    
+    print(funcion_uno.SumarPares(2, 10))
+    print(funcion_dos.Vectores(10))
+    print(funcion_tres.EsPrimo(23))
+    print(funcion_cuatro.Calculadora(30, 30))
+
+main()
+
