00bbf206192b510c3d55bdb17ecfafac15411594
Gamaliel Abad <gamaliel.abad@cua.uam.mx>
2023-08-11 18:36:27 -0600

    Incorporación_Función_Cinco

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 """En la UAM"""
 
+
 import funcion_uno 
 import funcion_dos
 import funcion_tres
 import funcion_cuatro
-     
+import funcion_cinco     
     
 def main():
     
@@ -12,6 +13,6 @@ def main():
     print(funcion_dos.Vectores(10))
     print(funcion_tres.EsPrimo(23))
     print(funcion_cuatro.Calculadora(30, 30))
-
+    print(funcion_cinco.Promedio_Calificaciones())
 main()
 
